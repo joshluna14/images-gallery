dc055307fed35b86d9942100e40ed6e9e77564ca
Joshua Luna <joshua.luna@ticketmaster.com>
2022-01-09 10:33:44 -0800

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
+import Search from "./components/search";
+
 
 const App = () => {
+  const [word, setWord] = useState('');
+  
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }
+
+
   return (
     <div>
      <Header title="Images Gallery"/> 
+     <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
